12fd36f511307cc6147d511a48b82047b6e4e757
Paul Elisha <ireajao@yahoo.com>
2026-01-22 17:39:56 +0100

    fix vercel isues

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-// This file exposes the Express app to Vercel's serverless runtime.
-// Vercel's @vercel/node builder will compile TypeScript in the api/ directory
-// so you can keep .ts imports in your source files.
-
-import app from "../app.js";
-
-export default app;
